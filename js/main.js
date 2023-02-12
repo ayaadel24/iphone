5f736387c507257d58ce75e490c893af15d3a8bd
Aya Adel <109903418+ayaadel24@users.noreply.github.com>
2023-02-12 22:31:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let pro = document.querySelector(".photo");
+let aya =  document.querySelector(".aya");
+function phones(phone)
+{
+    pro.src = phone;
+}
+function colors(color)
+{
+    aya.style.background = color;
+}
